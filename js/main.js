d9b6515cee092e3d917f936044b9ce57673c7049
lfvldvn <37988751+lfvldvn@users.noreply.github.com>
2022-06-12 04:18:44 +0000

    main

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const LinksSocialMedia = {
+    linkedin: 'in/alef/',
+    github: 'lfvldvn',
+    facebook: 'alefvaldevinobarboza',
+    twitter: 'lfvldvn',
+    instagram: 'lfvldvn',
+    whatsapp: ''
+    
+}
+
+function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+        //alert(li.children[0].href)
+    }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        //userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userPhoto.src = data.avatar_url
+        userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
