08a9e644a98776c2839a80d775d126c1dbf41be6
lfvldvn <37988751+lfvldvn@users.noreply.github.com>
2022-06-12 22:20:51 +0000

    main

--- js/main.js
+++ js/main.js
@@ -45,3 +45,29 @@ function onScroll() {
 if () {
     navigation.classList.add('scroll')
 }
+
+ var ancoras = document.querySelectorAll('a');
+        Array.from(ancoras).forEach(function (a) {
+            a.addEventListener('click', scroll);
+        });
+
+        function animarScroll(atual, destino) {
+            if (atual >= destino) return;
+            document.body.scrollTop = atual;
+            setTimeout(function () {
+                animarScroll(atual += 50, destino);
+            }, 50);
+        }
+
+        function scroll(e) {
+            e.preventDefault();
+            var id = this.getAttribute('href');
+            var el = document.querySelector(id);
+            var posicao = el.getBoundingClientRect().top;
+            animarScroll(this.scrollTop, posicao);
+        }
+
+        
+$(document).ready(function () {
+    $("html").niceScroll();
+});
