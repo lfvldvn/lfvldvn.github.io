50cc178fc7a680679e71790209f0e8efdcc21335
lfvldvn <37988751+lfvldvn@users.noreply.github.com>
2022-06-15 04:17:43 +0000

    main

--- js/main.js
+++ js/main.js
@@ -67,19 +67,3 @@ if () {
             animarScroll(this.scrollTop, posicao);
         }
 
-
-
-
-$(document).ready(function () {
-  $(".back-to-top").css("display", "none");
-$("body").scroll(function() {
-    if($(this).scrollTop() == 0){
-      $(".back-to-top").css("display", "none");
-    } else {
-      $(".back-to-top").css("display", "block");
-    }
-  });
-$(".back-to-top").click(function() {
-    $("html, body").animate({scrollTop: 0}, 800);
-   });
-});
