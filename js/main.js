25369ae4da25d2b7598e683dfdf7f754ca3e6d53
lfvldvn <37988751+lfvldvn@users.noreply.github.com>
2022-06-15 03:04:25 +0000

    main

--- js/main.js
+++ js/main.js
@@ -71,3 +71,18 @@ if () {
 $(document).ready(function () {
     $("html").niceScroll();
 });
+
+
+
+var stars = document.querySelectorAll('.star-icon');
+                  
+document.addEventListener('click', function(e){
+  var classStar = e.target.classList;
+  if(!classStar.contains('ativo')){
+    stars.forEach(function(star){
+      star.classList.remove('ativo');
+    });
+    classStar.add('ativo');
+    console.log(e.target.getAttribute('data-avaliacao'));
+  }
+});
